00b97ee61efa7613ad123a0841f721a78bf62c17
Andrhan Notepade <andrham2004@yandex.ru>
2024-10-03 10:07:09 +0800

    modified:   ER_model/Correct_ER_model.png 	new file:   Interface.py 	new file:   Migration/migrate.py

---
+++ Interface.py
@@ -0,0 +1,25 @@
+import CRUD
+import os
+
+def Mydef():
+    os.system('cls')
+    print("Для вставки новой книги нажмите 1")
+    print("Для удаления книги нажмите 2")
+    print("Для редактирования книги нажмите 3")
+    print("Для получения всех книги нажмите 4")
+    print("Для выхода нажмите 0")
+
+
+x = 1
+while(x != 0):
+    Mydef()
+    x = int(input())
+    os.system('cls')
+    if(x==1):
+        CRUD.InsertBook()
+    if(x==2):
+        CRUD.DeleteBook()
+    if(x==3):
+        CRUD.RedactBook()
+    if(x==4):
+        CRUD.PrintAllBook()
